eb3c754a32316c7a2900f10caa0325350f946c0d
Victor Inácio <chustrupgamer@gmail.com>
2022-02-11 15:00:01 -0300

    Created board and drawing figures of the board

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,83 @@ import p5 from 'p5';
 new p5(sketch);
 
 const canvasSize = 500;
+const linesOffset = canvasSize / 3;
+const generalStrokeWeight = 3;
+const board: string[][] = [
+    ['X', 'O', 'X'],
+    ['O', 'X', 'O'],
+    ['X', 'X', 'X'],
+];
 
 function sketch(p: p5) {
     p.setup = () => {
         p.createCanvas(canvasSize, canvasSize);
+        createOlds(p);
+        drawBoardFigures(p);
         drawLines(p);
-        // createOlds(p);
     };
 
     p.draw = () => {};
 }
 
 // Old === squares
-function createOlds(p: p5) {
-    const linesOffset = canvasSize / 3;
-
+function createOlds(p: p5): void {
     for (let y = 0; y <= canvasSize; y += linesOffset) {
         for (let x = 0; x <= canvasSize; x += linesOffset) {
-            p.stroke(0);
+            p.noStroke();
             p.fill(255);
             p.square(x, y, linesOffset);
         }
     }
 }
 
-function drawLines(p: p5) {
-    p.stroke(0);
-    p.strokeWeight(3);
+function drawBoardFigures(p: p5): void {
+    const padding: number = 70;
+    const squareSize: number = linesOffset;
 
-    const linesOffset = canvasSize / 3;
+    for (const rowIndex in board) {
+        const column: string[] = board[rowIndex];
+
+        for (const columnIndex in column) {
+            const figureToBeDrawn: string = column[columnIndex].toLowerCase();
+
+            if (figureToBeDrawn === 'x')
+                drawX([parseFloat(rowIndex), parseFloat(columnIndex)]);
+            if (figureToBeDrawn === 'o')
+                drawO([parseFloat(rowIndex), parseFloat(columnIndex)]);
+        }
+    }
+
+    function drawO([rowIndex, columnIndex]: number[]): void {
+        const circleX = columnIndex * squareSize + squareSize / 2;
+        const circleY = rowIndex * squareSize + squareSize / 2;
+        const circleRadius = squareSize - padding;
+        p.stroke(0);
+        p.strokeWeight(generalStrokeWeight);
+        p.circle(circleX, circleY, circleRadius);
+    }
+
+    function drawX([rowIndex, columnIndex]: number[]): void {
+        p.stroke(0);
+        p.strokeWeight(generalStrokeWeight);
+        const firstLineX1 = columnIndex * squareSize + padding / 2;
+        const firstLineY1 = rowIndex * squareSize + padding / 2;
+        const firstLineX2 = (columnIndex + 1) * squareSize - padding / 2;
+        const firstLineY2 = (rowIndex + 1) * squareSize - padding / 2;
+        p.line(firstLineX1, firstLineY1, firstLineX2, firstLineY2);
+        console.log(rowIndex * squareSize + padding / 2);
+
+        const secondLineX1 = (columnIndex + 1) * squareSize - padding / 2;
+        const secondLineY1 = rowIndex * squareSize + padding / 2;
+        const secondLineX2 = columnIndex * squareSize + padding / 2;
+        const secondLineY2 = (rowIndex + 1) * squareSize - padding / 2;
+        p.line(secondLineX1, secondLineY1, secondLineX2, secondLineY2);
+    }
+}
+
+function drawLines(p: p5): void {
+    p.stroke(0);
+    p.strokeWeight(generalStrokeWeight);
 
     for (let i = 1; i <= 2; i++) {
         const x = linesOffset * i;
